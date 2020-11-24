21044f9f19310122db918b2e8d8a30f227fa4699
Eder Rojas <edxavier05@gmail.com>
2020-11-24 14:32:21 +0000

    support para cargar datos dianmcos con selenium

--- flaskApp.py
+++ flaskApp.py
@@ -1,13 +1,30 @@
 
 from flask import Flask
 import requests
+from selenium import webdriver
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def lotoResults():
+def loto_results():
     res = requests.get("https://www.loto.com.ni/")
     return res.text, res.status_code
 
+
+@app.route("/previous")
+def loto_previous_results():
+    opt = webdriver.ChromeOptions()
+    opt.add_argument('headless')
+    browser = webdriver.Chrome(options=opt)
+
+    browser.get('https://www.loto.com.ni/resultados-anteriores/')
+    table = browser.find_element_by_xpath("//*[@id='resultados']")
+    html = table.get_attribute('innerHTML')
+    # html = browser.page_source
+    browser.close()
+    return html, 200
+
+
 if __name__ == "__main__":
-    app.run(debug=False, host= "0.0.0.0", port=7000)
+    app.run(debug=False, host="0.0.0.0", port=7000)
